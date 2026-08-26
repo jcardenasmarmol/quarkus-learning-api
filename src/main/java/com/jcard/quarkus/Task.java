1166f2a5cd655cb043bb57d021740efa53fa9f22
Jesús Cardeñas <jcardenasmarmol@gmail.com>
2026-08-26 18:37:16 +0200

    Add PostgreSQL and Flyway migrations

--- src/main/java/com/jcard/quarkus/Task.java
+++ src/main/java/com/jcard/quarkus/Task.java
@@ -2,6 +2,7 @@ package com.jcard.quarkus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import jakarta.validation.constraints.NotBlank;
@@ -10,7 +11,7 @@ import jakarta.validation.constraints.NotBlank;
 public class Task {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank(message = "Title is required")
     private String title;
